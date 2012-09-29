46f60abe68298216cd2d91d9ed14d2bfca3ae71c
Sato Daisuke <densuke@fuga.jp>
2012-09-29 09:28:37 +0900

    バッチファイル作成機能の追加 プロパティ読込サンプルの追加

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,12 +1,32 @@
 package ${package};
 
+import java.util.Properties;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-	public String getMessage() {
-        return "こんにちは世界!";
+	public static String getMessage(String name) {
+        return "こんにちは" + name;
 	}
+
+	public static void main(String[] args) {
+		/* プロパティファイルの読み込み */
+		final Properties prop = new Properties();
+		try {
+			prop.load(App.class.getClassLoader().getResourceAsStream("Sample.properties"));
+		} catch(Exception e) {
+			System.err.println("プロパティファイルの読み込みに失敗しました");
+			e.printStackTrace();
+		}
+
+		String first = prop.getProperty("firstname");
+		String last = prop.getProperty("lastname");
+
+		System.out.println(getMessage(first + " " + last));	
+	}
+
 }
+
