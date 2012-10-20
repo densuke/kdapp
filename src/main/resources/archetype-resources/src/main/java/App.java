b198d124822a14925750989032a79a250a14e94d
Sato Daisuke <densuke@fuga.jp>
2012-10-21 08:14:05 +0900

    ソース配置場所の修正、XMLタグのミスを修正

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,80 +1,10 @@
 package ${package};
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Properties;
-
-
-/**
- * Apache Derbyを使うサンプルです。
- *
- */
 public class App 
 {
 
 	public static void main(String[] args) {
-		/* プロパティファイルの読み込み */
-		final Properties prop = new Properties();
-		try {
-			prop.load(App.class.getClassLoader().getResourceAsStream("ja_JP.properties"));
-		} catch(Exception e) {
-			System.err.println("プロパティファイルの読み込みに失敗しました");
-			e.printStackTrace();
-		}
-		Connection con = null;
-		try {
-			Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
-		} catch (Exception e)
-		{
-			
-		}
-		try {
-		 con = DriverManager.getConnection("jdbc:derby:sample;create=true");
-		} catch(Exception e) {
-			
-		}
-		String first = prop.getProperty("firstname");
-		String last = prop.getProperty("lastname");
-		System.out.println("プロパティ読み込み: " + first + " " + last);
-		
-		try {
-			Statement st = con.createStatement();
-	        try { 
-				// テーブルの初期化 このサンプルでは毎回作り直しをしています
-	            st.executeUpdate("DROP TABLE HOGE"); 
-	        } catch (SQLException e) { 
-	        	// テーブルが存在しないときはここにいきます。が、なければいいのでこのままスルーします
-	        } 
-	        System.err.println("create");
-			st.executeUpdate("create table HOGE ( firstname varchar(32), lastname varchar(32) )");
-	        System.err.println("insert");			
-			PreparedStatement ps = con.prepareStatement("insert into HOGE values ( ? , ? )");
-			ps.setString(1, first + "SQL");
-			ps.setString(2, last + "SQL");
-			ps.executeUpdate();
-	        System.err.println("fetch");			
-			
-			ResultSet r = st.executeQuery("select firstname, lastname from HOGE");
-			r.next();
-			System.out.println("SQL取得結果: " + r.getString(1) + " " + r.getString(2));
-		} catch(Exception ex) {
-			System.out.println("caught");
-			ex.printStackTrace();
-			
-		}
-
-		System.out.println("こんにちは " + first + " " + last);	
-		
-		try {
-			DriverManager.getConnection("jdbc:derby:;shutdown=true");
-		} catch(Exception e) {
-			System.out.println("OK");
-		}
-		
+	    System.err.println("直接jarを起動してもだめですよ。");
 	}
 
 }
